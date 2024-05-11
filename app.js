08fc56bd1667017cd8352d3c2ff5062f95655e4c
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air.local>
2024-05-12 02:26:42 +0530

    port 4k

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 4000;
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose = require('mongoose');
